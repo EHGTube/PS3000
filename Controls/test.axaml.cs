a195ab67a25923b5dc7e9faac4d5e2f085c0e0fc
l.kuhn <l.kuhn@ehg-steinberg.de>
2024-12-14 22:24:42 +0100

    First UserControl Test

---
+++ Controls/test.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PS3000;
+
+public partial class test : UserControl
+{
+    public test()
+    {
+        InitializeComponent();
+    }
+}
